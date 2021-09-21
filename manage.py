17847b5730583aa06ab06acc728474f69522b89d
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2021-09-20 21:20:19 -0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_script._compat import text_type
 
-from .app import app
+from app import app
 from .database.models import db
 
 migrate = Migrate(app, db)
