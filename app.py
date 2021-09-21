4751a10759f3ec461cd7ed776326f636b54b82f4
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2021-09-21 01:01:40 -0600

    make it better

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-from .models import Movie_Actor, db_drop_and_create_all, setup_db, Movie, Actor, db
-from .auth import AuthError, requires_auth
+from models import Movie_Actor, db_drop_and_create_all, setup_db, Movie, Actor, db
+from auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
