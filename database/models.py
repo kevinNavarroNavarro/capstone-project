11d3e71119dcc95f7c13bc5308a80322e71e82be
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2021-09-20 23:16:52 -0600

    Update models.py

--- database/models.py
+++ database/models.py
@@ -7,7 +7,7 @@ DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
 DB_USER = os.getenv('DB_USER', 'postgres')  
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')  
 DB_NAME = os.getenv('DB_NAME', 'capstone')  
-database_path = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 db = SQLAlchemy()
 
 '''
