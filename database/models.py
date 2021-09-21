231564f5cf0d03bc7d73012a8a2a5ba0e808f7ca
kevinNavarroNavarro <40080096+kevinNavarroNavarro@users.noreply.github.com>
2021-09-20 22:13:47 -0600

    changes

--- database/models.py
+++ database/models.py
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 setup_db(app)
 '''
 def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://localhost:5432/capstone'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
